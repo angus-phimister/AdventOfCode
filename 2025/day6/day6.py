fcd6406b7e6a69112b3219483c80dd4ed38ec7b0
aphimister10 <98757508+aphimister10@users.noreply.github.com>
2025-12-06 15:54:18 +0000

    basically there

--- 2025/day6/day6.py
+++ 2025/day6/day6.py
@@ -1,20 +1,47 @@
-with open("2025/day6/input.txt") as f:
+with open("2025/day6/test.txt") as f:
     input = f.read().split('\n')
-    input = [[c for c in x.split(" ") if c !=""] for x in input]
+    input_1 = [[c for c in x.split(" ") if c !=""] for x in input]
 
-total = 0 
-for i in range(len(input[0])):
-    if input[-1][i] == "+":
-        ind_total=0
-        for j in range(3):
-            ind_total += int(input[j][i])
-    else:
-        ind_total=1
-        for j in range(3):
-            ind_total *= int(input[j][i])    
+
+
+# total = 0 
+# for i in range(len(input_1[0])):
+#     if input_1[-1][i] == "+":
+#         ind_total=0
+#         for j in range(4):
+#             ind_total += int(input_1[j][i])
+
+#     else:
+#         ind_total=1
+#         for j in range(4):
+#             ind_total *= int(input_1[j][i])   
+
     
-    total += ind_total
+#     total += ind_total
 
-print(total) # 17239378288 too low 
+# print(total)
+
+# Part 2
+nums = input[:3]
+operands = [x for x in input[-1].split(" ") if x!=""]
+
+
+number_sets = []
+current_number_set = []
+for i in range(len(nums[0])-1,0,-1):
+    number = ""
+    for j in range(3):
+        number+=nums[j][i]
+    number = number.replace(" ", "")
+
+    if len(number)>0:
+        number = int(number)
+        current_number_set.append(number)
+
+    else:
+        number_sets.append(current_number_set)
+        current_number_set = []
+
+
 breakpoint()
         
